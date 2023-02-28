6f57818934d4ff34b7677d2280ab8cefb97ba0a5
Huseyin Asik <hasik@k2m08s09.42kocaeli.com.tr>
2023-02-28 17:19:19 +0300

    shell

---
+++ asd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+int main(int a, char **v, char **e)
+{
+    while(*e)
+    {
+        printf("%s\n", *e);
+        *e++;
+    }
+}
+*/
---
+++ readline.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/*
+int main(int argc, char **argv)
+{
+    char *input;
+    input = readline("Enter a command: ");
+    printf("You entered: %s\n", input);
+    free(input);
+    return (0);
+}
+*/
